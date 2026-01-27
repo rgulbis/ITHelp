8eea937a146d8a7727ff448614a61a636c9494e5
rgulbis <rgubis41@gmail.com>
2026-01-27 11:19:19 +0200

    seppareta usera

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,23 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
+    public function canAccessPanel(Panel $panel): bool
+    {
+        if ($panel->getId() === 'admin' && $panel->getId() === $this->role) {
+            return true;
+        } else if($panel->getId() === 'user' && $panel->getId() === $this->role) {
+            return true;
+        }
+    }
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
